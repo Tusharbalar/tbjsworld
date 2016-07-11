faecc44805070c243ac49825c7fc40499a2ba905
TusharBalar <balar.tushar1@gmail.com>
2016-07-11 21:54:07 +0530

    Add favicon

---
+++ index.php
@@ -0,0 +1,14 @@
+<?php header( 'Location: /index.html' ) ;  ?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <link href="node_modules/bootstrap/dist/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.4/jquery.min.js"></script>
+  <script src="node_modules/jquery/dist/jquery.min.js"></script>
+  <scirpt src="node_modules/bootstrap/dist/js/bootstrap.min.js"></script>
+</head>
+
+<body></body>
+
+</html>
